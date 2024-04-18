591545184afc889ea1e118e55025aff0c12549c9
zebaunnisa03 <108784752+zebaunnisa03@users.noreply.github.com>
2024-04-18 20:05:08 +0530

    Update ID3Algorithm.py

--- ID3Algorithm.py
+++ ID3Algorithm.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
 import numpy as np
-nparray=np.array([[1,2,3],[1,2,3]])
-print(nparray)
+nparray=np.array([[1,2,3,4],[5,6,7,8]])
+print(tf.reduce_sum(nparray))
+print(type(nparray))
